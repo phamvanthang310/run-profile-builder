25b0cde776aa09df1f698fabf6571201a4aea7d3
ThangPham <thangpham@kms-technology.com>
2017-11-20 17:04:08 +0700

    Minor update for KMS deployment

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 scp target/release-supporting-tool-0.1.0-SNAPSHOT.jar masuser@config.dolphin.lexisnexisrisk.com:/lexisnexis/application/release-tool
 
-scp src/webui/dist masuser@config.dolphin.lexisnexisrisk.com:/lexisnexis/application/release-tool
+scp -r src/webui/dist masuser@config.dolphin.lexisnexisrisk.com:/lexisnexis/application/release-tool
 
 java -Djavax.net.ssl.trustStore=./jssecacerts -jar release-supporting-tool-0.1.0-SNAPSHOT.jar org.springframework.boot.loader.PropertiesLauncher --jira.token=UGhhbVRoMDFAcmlzazpIQG5kc29tZTMxMA==
+
+node dist/server.js
